840567482e6b721dbdc13bbdc754466e0d90dd61
bang-dai <bang.dai2@gmail.com>
2017-11-09 00:26:12 +0100

    Add search feature for product

--- src/AppBundle/Controller/ProductController.php
+++ src/AppBundle/Controller/ProductController.php
@@ -3,7 +3,9 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Product;
+use AppBundle\Form\SearchType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -26,5 +28,23 @@ class ProductController extends Controller
         }
 
         return $this->render(':product:show.html.twig', ['product' => $product]);
+    }
+
+    /**
+     * @Route("/search", name="product.search")
+     */
+    public function searchAction(Request $request)
+    {
+        $form = $this->createForm(SearchType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $products = $em->getRepository(Product::class)->search($data['search']);
+            
+            return $this->render(':product:index.html.twig', ['products' => $products]);
+        }
+
+        return $this->render(':layout:search.html.twig', ['form' => $form->createView()]);
     }
 }
