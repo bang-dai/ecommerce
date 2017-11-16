e474ea29a96a817e54b8f8644635ec73164e0867
bang-dai <bang.dai2@gmail.com>
2017-11-16 12:48:13 +0100

    Update twig extension for vat

--- src/AppBundle/Twig/AppExtension.php
+++ src/AppBundle/Twig/AppExtension.php
@@ -7,12 +7,18 @@ class AppExtension extends \Twig_Extension
     public function getFilters()
     {
         return array(
-            new \Twig_SimpleFilter('vat', array($this, 'computeVat'))
+            new \Twig_SimpleFilter('ttc', array($this, 'computeTTC')),
+            new \Twig_SimpleFilter('vat', array($this, 'computeVAT'))
         );
     }
 
-    public function computeVat($priceHT, $vat)
+    public function computeTTC($priceHT, $vat)
     {
-        return round($priceHT / $vat, 2);
+        return round($priceHT * (1 + $vat/100), 2);
+    }
+
+    public function computeVAT($priceHT, $vat)
+    {
+        return round($priceHT * ($vat/100), 2);
     }
 }
